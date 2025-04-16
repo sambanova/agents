7fe300c8d65a84752c1ef340f42723cab4aab96f
snova-jorgep <jorge.piedrahita@sambanovasystems.com>
2025-04-16 12:41:33 -0500

    add image analysis tool

--- backend/api/agents/sambanova_knowledge/llamastack_sambanova_knowledge.py
+++ backend/api/agents/sambanova_knowledge/llamastack_sambanova_knowledge.py
@@ -5,6 +5,7 @@ import time
 import json
 from utils.logging import logger
 
+import llama_stack_client
 from llama_stack_client import LlamaStackClient
 from llama_stack_client.lib.agents.agent import Agent
 from llama_stack_client.lib.agents.event_logger import EventLogger
@@ -16,6 +17,68 @@ from llama_stack_client.types.agents.turn_response_event_payload import (
     InferenceStep,
 )
 
+# TODO add examples to routing
+
+class UnstructuredDataTools():
+    def __init__(self, client):
+        self.files={}
+        self.client = client
+        self.vision_model = "sambanova/Llama-3.2-11B-Vision-Instruct"
+        # TODO set from model registry map 
+        
+    def save_files(self, b64_files: list):
+        self.files = b64_files
+        
+    def get_files(self):
+        return self.files    
+    
+    def get_image_files(self):
+        image_formats=["png","jpg","jpeg"]
+        image_files = []
+        for filename, data in self.get_files().items():
+            if filename.lower().split('.')[-1] in image_formats:
+                image_files.append(data)
+        return image_files
+    
+    # Tools that requires access to files 
+    def image_analysis(self, query: str) -> str:
+        """
+        Runs image analysis tool.
+
+        :param image: str
+        :param query:  str
+        
+        Returns:
+            str: the response of analyse the image with the query 
+            
+        """ 
+        
+        files = self.get_image_files()
+        if len(files)==0:
+            return "The user has not attached any image"
+        else:    
+            response = self.client.inference.chat_completion(
+                model_id=self.vision_model,
+                messages=[
+                    {
+                        "role": "user",
+                        "content":{
+                            "type": "image",
+                            "image": {
+                                "url": {
+                                    "uri": files[0]
+                                }
+                            }
+                        }
+                    },
+                    {
+                        "role": "user",
+                        "content": query,
+                    }
+                ],
+            )
+
+            return response.completion_message.content
 
 def estimate_tokens_regex(text: str) -> int:
     """
@@ -28,27 +91,9 @@ def create_http_client(api_key=None):
     return LlamaStackClient(
         base_url=f"http://{os.environ['LLAMA_STACK_HOST']}:{os.environ['LLAMA_STACK_PORT']}",
         provider_data={"sambanova_api_key": api_key},
-    )
+    ) 
 
-
-# Example tool definition
-def code_executor(query: str) -> str:
-    """
-    Runs code executor tool.
-
-    :param query: code expression to run in sandbox environment.
-
-    Returns:
-        str: the response of evaluating the expression
-    """
-    try:
-        result = eval(query)
-    except Exception as e:
-        result = f"error when evaluating expression {query} - {e}"
-    return True
-
-
-def create_agent(client, model):
+def create_agent(client, model, docs_tools):
     """
     initialize agent
     """
@@ -56,19 +101,21 @@ def create_agent(client, model):
         client,
         model=model,
         instructions="""
-        You are an expert in composing functions. You are given a question and a set of possible functions.
+        You are a helpful assistant that can access external functions. The responses from these function calls will be appended to this dialogue. Please provide responses based on the information from these function calls.
+        You are given a question and a set of possible functions.
         Based on the question, you will need to make one or more function/tool calls to achieve the purpose.
-        If none of the function can be used, point it out. If the given question lacks the parameters required by the function,
-        also point it out. You should only return the function call in tools call sections.
-        If you decide to invoke any of the function(s), you MUST put it in the format of [func_name1(params_name1=params_value1, params_name2=params_value2...), func_name2(params)]
-        You SHOULD NOT include any other text in the response.
+        If none of the function can be used, point it out. If the given question lacks the parameters required by the function, also point it out.
+        If you decide to invoke any of the function(s), you MUST put it in a valid json format, the format is:
+        [{{"name": func_1, "parameters": dictionary of argument name and its value}}, {{"name": func_2, "parameters": dictionary of argument name and its value}}]<eot_id>
+        If you decide to call a function you SHOULD NOT include any other text in the response.
+        Avoid generating extra scape characters when calling a function (like `\\n`, multiple backslashes `\\\\` and other format that could generate problems when parsing the arguments of the function call, ensure you add the '<eot_id>' when calling a tool
         
-        You are a code agent, use your tool to execute code. Remember, always give the final answer using the tool result, if the results are empty or you receive an error, let the user know.
-        """,
+        You are a general agent, you can use your tools to execute code or analyse and interpret images. Remember, always give the final answer using the tool result, if the results are empty or you receive an error, let the user know.
+        """, #TODO improve prompting to avoid keep executing tool calls
         sampling_params={
-            "strategy": {"type": "top_p", "temperature": 0.1, "top_p": 0.9},
+            "strategy": {"type": "top_p", "temperature": 0.1, "top_p" : 0.1},
         },
-        tools=["builtin::code_interpreter"],
+        tools=["builtin::code_interpreter", docs_tools.image_analysis],
         enable_session_persistence=False,
     )
     return agent
@@ -85,7 +132,13 @@ def call(thread_config, query, docs, files_b64, api_key):
     redis_client = config["redis_client"]
 
     client = create_http_client(api_key)
-    agent = create_agent(client, config["model"])
+    docs_tools = UnstructuredDataTools(client)
+    
+    if files_b64 is not None:
+        docs_tools.save_files(files_b64)
+    agent = create_agent(client, config["model"], docs_tools)
+    
+    #TODO add document to messages
 
     # agent turn execution
     response = agent.create_turn(
@@ -169,7 +222,7 @@ def call(thread_config, query, docs, files_b64, api_key):
                 event_message = {
                     "event_object": [
                         {
-                            "name": "Code execution step",
+                            "name": "Code execution step", # TODO use task here
                             "Tool calls": f"```json\n{tool_calls}\n```",
                             "Tool Execution results": f"```json\n{tool_responses}\n```",
                         },
@@ -206,5 +259,7 @@ def call(thread_config, query, docs, files_b64, api_key):
                     f"agent_thoughts:{config['user_id']}:{config['conversation_id']}"
                 )
                 redis_client.publish(channel, json.dumps(intermediate_message))
+                
+    #TODO delete client, agent, and vault objects
 
     return response, assistant_metadata
